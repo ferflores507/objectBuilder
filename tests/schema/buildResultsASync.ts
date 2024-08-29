ddd1445095039cf41e8fb736dfb301ecfe881f39
Fernando <ferflores507@gmail.com>
2024-08-28 22:18:30 -0500

    added new function expectToEqualAsync

--- tests/schema/buildResultsASync.ts
+++ tests/schema/buildResultsASync.ts
@@ -1,5 +1,19 @@
+import { expect } from "vitest"
 import { ObjectBuilder, Schema } from "../.."
 
 export const buildResultsAsync = async (builder: ObjectBuilder, schema: Schema) => {
     return [builder.build(schema), await builder.buildAsync(schema)]
+}
+
+export type Case = {
+    source: Record<string, any>
+    schema: Schema
+    expected: any
+}
+
+export const expectToEqualAsync = async (caseArg: Case) => {
+    const { source, schema, expected } = caseArg
+    const results = await buildResultsAsync(new ObjectBuilder(source), schema)
+
+    expect(results).toEqual([expected, expected])
 }
