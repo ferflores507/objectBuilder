30f6ce21daa96b81a2f281b2f9adb07238c6c4b0
Fernando <ferflores507@gmail.com>
2024-10-18 16:09:06 -0500

    encapsulated map.size in get length

--- src/helpers/Queue.ts
+++ src/helpers/Queue.ts
@@ -2,13 +2,17 @@ export class Queue {
     offset = 0
     map = new Map()
 
+    get length() {
+        return this.map.size
+    }
+
     enqueue(item: any) {
-        const current = this.offset + this.map.size
+        const current = this.offset + this.length
         this.map.set(current, item)
     }
 
     dequeue() {
-        if(this.map.size > 0) {
+        if(this.length > 0) {
             const value = this.map.get(this.offset)
             this.map.delete(this.offset++)
 
