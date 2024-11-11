a7b5c517fa53740c05be1058b4d3830d1d71a844
Fernando <ferflores507@gmail.com>
2024-11-11 08:21:11 -0500

    updated type CaseBase to union and renamed CaseOptions, options can be used directly from arg

--- tests/schema/buildResultsASync.ts
+++ tests/schema/buildResultsASync.ts
@@ -2,28 +2,20 @@ import { expect } from "vitest"
 import { Schema } from "../.."
 import { BuilderOptions, SchemaTaskResultBuilder } from "../../src/builders/SchemaTaskResultBuilder"
 
-export const buildResultsAsync = async (caseArg: CaseBase) => {
-    const { source, schema, options } = caseArg
+type CaseOptions = Partial<BuilderOptions> & { schema: Schema }
+
+export const buildResultsAsync = async (options: CaseOptions) => {
+
     const builder = new SchemaTaskResultBuilder()
-        .with({
-            store: source,
-            ...options
-        })
 
-    return [builder.with({ schema }).build(), await builder.with({ schema }).buildAsync()]
+    return [builder.with(options).build(), await builder.with(options).buildAsync()]
 }
 
-export type CaseBase = {
-    source?: Record<string, any>
-    schema: Schema
-    options?: BuilderOptions
-}
-
-export type Case = { expected: any } & CaseBase
+export type Case = { expected: any } & CaseOptions
 
 export const expectToEqualAsync = async (caseArg: Case) => {
-    const { expected } = caseArg
-    const results = await buildResultsAsync(caseArg)
+    const { expected, ...options } = caseArg
+    const results = await buildResultsAsync(options)
 
     expect(results).toEqual([expected, expected])
 }
