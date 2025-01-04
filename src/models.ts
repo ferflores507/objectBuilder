f83f5ccbc01c86ca0629e26f2cdedfc3531a782a
Fernando <ferflores507@gmail.com>
2025-01-04 11:11:34 -0500

    update(models): added new property init as Schema

--- src/models.ts
+++ src/models.ts
@@ -71,6 +71,7 @@ export type Schema = Partial<{
     includes: SchemaDefinition | SchemaPrimitive
     increment: string
     import: string
+    init: Schema
     join: SchemaDefinition | SchemaPrimitive
     log: SchemaDefinition
     not: SchemaDefinition
