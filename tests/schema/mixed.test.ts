f19cf9927233e0b07891316ee37c1f948903a726
Fernando <ferflores507@gmail.com>
2025-02-02 17:57:45 -0500

    update(varios/compareOptions): added boolean option to convert value to boolean first then to number (to sort by 0 or 1), added test

--- tests/schema/mixed.test.ts
+++ tests/schema/mixed.test.ts
@@ -9,82 +9,142 @@ import { Queue } from '../../src/helpers/Queue'
 import { TaskBuilder } from '../../src/builders/TaskBuilder'
 import { Propiedades } from '../../src/models'
 
-describe("sort by multiple options", () => {
-  const cases = [
-    {
-      name: "sort by id then by name",
-      items: [
-        [3, "b"],
-        [3, "a"],
-        [2, "b"],
-        [2, "a"],
-        [1, "b"],
-        [1, "a"]
-      ],
-    },
-    {
-      name: "sort by id then by name descending",
-      items: [
-        [3, "a"],
-        [3, "b"],
-        [2, "a"],
-        [2, "b"],
-        [1, "a"],
-        [1, "b"],
-      ],
-      descending: ["name"],
-    },
-    {
-      name: "sort by id descending then by name",
-      items: [
-        [1, "b"],
-        [1, "a"],
-        [2, "b"],
-        [2, "a"],
-        [3, "b"],
-        [3, "a"],
-      ],
-      descending: ["id"]
-    },
-    {
-      name: "sort by id descending then by name descending",
-      items: [
-        [1, "a"],
-        [1, "b"],
-        [2, "a"],
-        [2, "b"],
-        [3, "a"],
-        [3, "b"],
-      ],
-      descending: ["id", "name"]
-    }
-  ]
-  
-  test.each(cases)("$name", async ({ items, descending }) => {
-    const initial = items.map(([id, name]) => ({ id, name }))
+describe("sort by", () => {
+  test("sort by completed (boolean) then by completed (numeric) descending", async () => {
+    const items = Array.from(Array(6).keys()).map(i => i + 1)
+    const zeros = [0, 0, 0]
+    
     await expectToEqualAsync({
-      initial,
+      initial: items.toSpliced(3, 0, ...zeros).map(completed => ({ completed })),
       schema: {
         sortBy: [
           {
-            const: {
-              path: "id",
-              type: "numeric",
-              descending: descending?.includes("id")
+            propiedades: {
+              path: "completed",
+              type: "boolean"
             }
           },
           {
-            const: {
-              path: "name",
-              descending: descending?.includes("name")
+            propiedades: {
+              path: "completed",
+              type: "numeric",
+              descending: true
             }
           }
         ]
       },
-      expected: initial.toReversed()
+      expected: [...zeros, ...items.reverse()].map(completed => ({ completed }))
     })
   })
+
+  describe("sort by multiple options", () => {
+    const cases = [
+      {
+        name: "sort by id then by name",
+        items: [
+          [3, "b"],
+          [3, "a"],
+          [2, "b"],
+          [2, "a"],
+          [1, "b"],
+          [1, "a"]
+        ],
+      },
+      {
+        name: "sort by id then by name descending",
+        items: [
+          [3, "a"],
+          [3, "b"],
+          [2, "a"],
+          [2, "b"],
+          [1, "a"],
+          [1, "b"],
+        ],
+        descending: ["name"],
+      },
+      {
+        name: "sort by id descending then by name",
+        items: [
+          [1, "b"],
+          [1, "a"],
+          [2, "b"],
+          [2, "a"],
+          [3, "b"],
+          [3, "a"],
+        ],
+        descending: ["id"]
+      },
+      {
+        name: "sort by id descending then by name descending",
+        items: [
+          [1, "a"],
+          [1, "b"],
+          [2, "a"],
+          [2, "b"],
+          [3, "a"],
+          [3, "b"],
+        ],
+        descending: ["id", "name"]
+      }
+    ]
+    
+    test.each(cases)("$name", async ({ items, descending }) => {
+      const initial = items.map(([id, name]) => ({ id, name }))
+      await expectToEqualAsync({
+        initial,
+        schema: {
+          sortBy: [
+            {
+              const: {
+                path: "id",
+                type: "numeric",
+                descending: descending?.includes("id")
+              }
+            },
+            {
+              const: {
+                path: "name",
+                descending: descending?.includes("name")
+              }
+            }
+          ]
+        },
+        expected: initial.toReversed()
+      })
+    })
+  })
+
+  test("sort by name then by id", async () => {
+    const items = [
+      [3, "b"],
+      [2, "b"],
+      [1, "b"],
+      [3, "a"],
+      [2, "a"],
+      [1, "a"],
+    ].map(([id, name]) => ({ id, name }))
   
+    await expectToEqualAsync({
+      schema: {
+        const: items,
+        sortBy: [
+          {
+            propiedades: {
+              path: "name",
+            }
+          },
+          {
+            propiedades: {
+              path: "id",
+              type: "numeric",
+            }
+          }
+        ]
+      },
+      expected: items.toReversed()
+    })
+  
+  })
 })
 
 test("propiedadesAsync", async () => {
